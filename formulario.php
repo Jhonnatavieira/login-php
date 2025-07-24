0d32adaa747b7505ca9bd7543fb2ada4c49ff139
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-07-24 15:58:22 -0300

    fix modal

--- formulario.php
+++ formulario.php
@@ -14,7 +14,7 @@
                     <div class="row">
                         <div class="col">
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal2">
+                                data-bs-target="#exampleModal">
                                 Cadastrar cliente
                             </button>
                         </div>
